7de06c5e9a826f23665a467b1c9b0db4c7312aee
Nawaf <nawafalqari13@gmail.com>
2021-10-08 17:21:42 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
 
 def read(fname):
@@ -6,21 +6,27 @@ def read(fname):
 
 setup(
     name='jsonwriter',
-    version='0.0.5',
+    version='0.1.4',
     description='Easy JSON Writer',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     author='Nawaf Alqari',
-    author_email='nawafalqari13@gmail.com',
-    packages=['jsonwriter'],
+    author_email='contact@nawaf.cf',
+    packages=find_packages(where='jsonwriter'),
+    python_requires='>=3.6',
     license='MIT',
     url = 'https://github.com/nawafalqari/jsonwriter',
+    project_urls = {
+        'Github': 'https://github.com/nawafalqari/jsonwriter',
+        'Issues': 'https://github.com/nawafalqari/jsonwriter/issues',
+    },
     keywords=['jsondb', 'json database', 'jsondatabase', 'json editor', 'json', 'json writer'],
     classifiers=[
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
   ],
 )
+
+# python setup.py sdist
+# twine upload dist/jsonwriter-[version].tar.gz
